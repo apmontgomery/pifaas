0bce7a0ccf79aa11769f98c16048cfb542facf9c
Alex Montgomery <alex@mon.tg>
2022-04-08 23:47:40 +0800

    change rpi-gpio to onoff

--- mystars/handler.js
+++ mystars/handler.js
@@ -1,22 +1,13 @@
 'use strict'
-const gpio = require('rpi-gpio').promise
+const gpio = require('onoff').Gpio
 
 module.exports = async (event, context) => {
-  let error = 'none'
-  let gpiovalue = 'none'
-  try {
-    await gpio.setup(7, gpio.DIR_IN)
-    gpiovalue = await gpio.read(7)
-  } catch (err) {
-    error = err
-  }
 
   const result = {
     'body': JSON.stringify(event.body),
     'content-type': event.headers["content-type"],
     'other': 'morer',
-    error,
-    gpiovalue
+    gpiovalue: gpio.accessible
   }
 
   return context
