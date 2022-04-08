2ef6b856cf88fc180e1809c53024732a9d2e3318
Alex Montgomery <alex@mon.tg>
2022-04-08 23:20:44 +0800

    check gpio

--- mystars/handler.js
+++ mystars/handler.js
@@ -1,10 +1,22 @@
 'use strict'
+const gpio = require('rpi-gpio').promise
 
 module.exports = async (event, context) => {
+  let error = 'none'
+  let gpiovalue = 'none'
+  try {
+    await gpio.setup(7, gpio.DIR_IN)
+    gpiovalue = await gpio.read(7)
+  } catch (err) {
+    error = err
+  }
+
   const result = {
     'body': JSON.stringify(event.body),
     'content-type': event.headers["content-type"],
-    'other': 'morer'
+    'other': 'morer',
+    error,
+    gpiovalue
   }
 
   return context
