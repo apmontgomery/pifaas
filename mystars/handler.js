64b4f8b6ce02da23ed151187a04c02e796916e46
Alex Montgomery <alex@mon.tg>
2022-04-15 13:08:58 +0800

    remove readdir

--- mystars/handler.js
+++ mystars/handler.js
@@ -1,14 +1,17 @@
 'use strict'
 const fs = require('fs')
+const os = require('os)
+const userInfo = os.userInfo()
 
 module.exports = async (event, context) => {
 
-  const sy = fs.readdirSync('/sys/class/gpio')
+  // const sy = fs.readdirSync('/sys/class/gpio')
   const result = {
     'body': JSON.stringify(event.body),
     'content-type': event.headers["content-type"],
     'other': 'morerer',
-    sy
+    // sy,
+    userInfo
   }
 
   return context
